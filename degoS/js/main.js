40078ee57dc9a321f59d3f71f2a5d4ecd7e32992
VikariiAngelina <linuli4ka@gmail.com>
2018-03-11 11:38:40 +0200

    slider

--- degoS/js/main.js
+++ degoS/js/main.js
@@ -1,6 +1,6 @@
-function printName(param,name){
-	param(name);
-}
-printName(function(name){
-	alert(name);
-},"Ivan");
+$(function () {
+	$('.sliderTop').slick({
+	arrows:false,
+	adaptiveHeight:true
+	});
+});
