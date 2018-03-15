adfb7f016dcd53beaa1c7575b4fe33e651acab6d
VikariiAngelina <linuli4ka@gmail.com>
2018-03-16 00:53:45 +0200

    added slider all

--- degoS/js/main.js
+++ degoS/js/main.js
@@ -1,6 +1,19 @@
 $(function () {
 	$('.sliderTop').slick({
 	arrows:false,
+	adaptiveHeight:true,
+	dots: false,
+  	infinite: true,
+ 	 autoplay:true,
+ 	 speed: 1000,
+ 	 fade: true,
+  	cssEase: 'linear'
+	});
+});
+
+$(function () {
+	$('.sliderBottom').slick({
+	arrows:true,
 	adaptiveHeight:true
 	});
 });
