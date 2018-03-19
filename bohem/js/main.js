2736cfb28172fd8f71c187a77345695c6297128d
VikariiAngelina <linuli4ka@gmail.com>
2018-03-19 19:02:49 +0200

    + header

--- bohem/js/main.js
+++ bohem/js/main.js
@@ -5,3 +5,14 @@ $(function () {
 	adaptiveHeight:true
 	});
 });
+
+$(function(){
+	$(".toggleMobMenu").on("click",function(){
+		$(".menuCont").stop().slideToggle();
+	});
+
+	$(".searchIcon").on("click",function(){
+		$(".header__form").stop().slideToggle();
+	});
+
+});
